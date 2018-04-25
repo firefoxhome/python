54e150cac6a0440d550d470cfba700533391ce77
firefoxhome <2683299672@qq.com>
2018-04-25 10:34:07 +0800

    Delete Note

--- public_upgrade821.py
+++ public_upgrade821.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 #coding=utf-8
 
 import os
@@ -31,6 +32,7 @@ import sys
 #2018-3-6  14:16  The test public net is replaced by IP 140.143.16.230
 #2018-3-6  16:23  Repeated downloading two checks on the MD5 check
 #2018-3-6  20:28  Modify the MD5, changlog link
+#2018-4-25 10:32  Delete Note
 
 
 def mymovefile(srcfile, dstfile):
@@ -44,17 +46,6 @@ def mymovefile(srcfile, dstfile):
 		print "move %s -> %s"%(srcfile, dstfile)
 
 def Schedule(a,b,c):
-
-    '''
-
-    a:已经下载的数据块
-
-    b:数据块的大小
-
-    c:远程文件的大小
-
-   '''
-
     per = 100.0 * a * b / c
 
     if per > 100 :
@@ -62,14 +53,6 @@ def Schedule(a,b,c):
         per = 100
 
     print '%d%%' % per,'已下载的大小:',a*b,'文件大小:',c
-
-    #print '已经下载的数据块:',a#,'\n'
-
-    #print '数据块的大小:',b#,'\n'
-
-    #print '远程文件大小:',c,'\n'
-
-    #print '已下载的大小:',a*b,'文件大小:',c
 
 def check(fs,fd):
     if fs == fd:
@@ -89,7 +72,6 @@ def check(fs,fd):
             #output = os.popen('ifconfig | grep eth | cut -c 39-65')
             output = os.popen('ifconfig | grep wlp2s0 | cut -c 39-65')
             mac =  output.read()
-            #print mac
             #str = 'http://ams.b-bug.org/mac/address.py?a=%s' %mac
             #str = 'http://ams.b-bug.org/mac/?a=%s' %mac
             str = 'http://140.143.16.230/mac/?a=%s' %mac
@@ -99,8 +81,7 @@ def check(fs,fd):
             #Crawl the content
             page = urllib2.urlopen(str)   
             contents = page.read()  
-            #print(contents)
-
+            
             url = 'https://canaan.io/downloads/software/avalon821/mm/latest/md5sums'
             md5 = os.path.join('/home/factory/Avalon-extras/scripts/factory','md5sums' )
 
@@ -117,8 +98,6 @@ def check(fs,fd):
             check2 = os.popen('md5sum MM821.mcs | cut -c 1-32')
             ck1 =  check1.read()
             ck2 =  check2.read()
-            #print ck1
-            #print ck2
 
             if ck1 == ck2:
                 print "MD5 Right"
@@ -137,161 +116,155 @@ def check(fs,fd):
 
 
 
+if __name__ == '__main__':
+    #Get links
+    #output = os.popen('ifconfig | grep eth | cut -c 39-56')
+    output = os.popen('ifconfig | grep wlp2s0 | cut -c 39-65')
+    mac =  output.read()
+    #str = 'http://ams.b-bug.org/mac/address.py?a=%s' %mac
+    #str = 'http://ams.b-bug.org/mac/?a=%s' %mac
+    str = 'http://140.143.16.230/mac/?a=%s' %mac
+    print "The URL is being accessed",str
 
-#Get links
-#output = os.popen('ifconfig | grep eth | cut -c 39-56')
-output = os.popen('ifconfig | grep wlp2s0 | cut -c 39-65')
-mac =  output.read()
-#print mac
-#str = 'http://ams.b-bug.org/mac/address.py?a=%s' %mac
-#str = 'http://ams.b-bug.org/mac/?a=%s' %mac
-str = 'http://140.143.16.230/mac/?a=%s' %mac
-print "The URL is being accessed",str
+    #Crawl the content
+    page = urllib2.urlopen(str)   
+    contents = page.read()  
 
-#Crawl the content
-page = urllib2.urlopen(str)   
-contents = page.read()  
+    if page.getcode() != 200:
+        os._exit()
+    else:
+        print "Server links Right"
 
-if page.getcode() != 200:
-    os._exit()
-else:
-    print "Server links Right"
+    print "The content of the obtained web page is",contents
 
-print "The content of the obtained web page is",contents
-
-print 'http header:/n', page.info() 
-print 'http status:', page.getcode() 
-
-
-result = re.search( r'("Download_link": ")(.*?)(")', contents, re.M|re.I)
-address = result.group(2)
-print  "The firmware download address is",address
-
-os.chdir('/home/factory/Avalon-extras')
-print "当前工作目录 : %s" % os.getcwd()
-os.system("ls")
+    print 'http header:/n', page.info() 
+    print 'http status:', page.getcode() 
 
 
-fd = os.open("version",os.O_RDWR)
-#version = os.popen('cat version | cut -c 1-4').read(fd,1)
-ver = os.read(fd,4)
-print "Local script version",ver
+    result = re.search( r'("Download_link": ")(.*?)(")', contents, re.M|re.I)
+    address = result.group(2)
+    print  "The firmware download address is",address
 
-result = re.search( r'("script_version": ")(.*?)(")', contents, re.M|re.I)
-net_ver = result.group(2)
-print "The latest script version of the server",net_ver
-
+    os.chdir('/home/factory/Avalon-extras')
+    print "当前工作目录 : %s" % os.getcwd()
+    os.system("ls")
 
 
-if ver == net_ver:
-    print "The version is already the latest"
-    #exit(0)
-    #print "The firmware is not required to be downloaded"
-else:
-    print "The version is not the latest"
+    fd = os.open("version",os.O_RDWR)
+    #version = os.popen('cat version | cut -c 1-4').read(fd,1)
+    ver = os.read(fd,4)
+    print "Local script version",ver
+
+    result = re.search( r'("script_version": ")(.*?)(")', contents, re.M|re.I)
+    net_ver = result.group(2)
+    print "The latest script version of the server",net_ver
 
 
-#step1 Back up the existing firmware
-#while True:
-os.chdir('/home/factory/Avalon-extras/scripts/factory')
-print "当前工作目录 : %s" % os.getcwd()
-os.system("ls")
+
+    if ver == net_ver:
+        print "The version is already the latest"
+        #exit(0)
+        #print "The firmware is not required to be downloaded"
+    else:
+        print "The version is not the latest"
 
 
-srcfile = '/home/factory/Avalon-extras/scripts/factory/MM821.mcs'
-dstfile = '/home/factory/canaan_factory/MM821.mcs'
-#Add dstfile = /home/factory/canaan_factory/MM821.mcs, solve the firmware movement is not covered.
+    #step1 Back up the existing firmware
+    os.chdir('/home/factory/Avalon-extras/scripts/factory')
+    print "当前工作目录 : %s" % os.getcwd()
+    os.system("ls")
+
+
+    srcfile = '/home/factory/Avalon-extras/scripts/factory/MM821.mcs'
+    dstfile = '/home/factory/canaan_factory/MM821.mcs'
+    #Add dstfile = /home/factory/canaan_factory/MM821.mcs, solve the firmware movement is not covered.
 
 
 
 
-if os.path.exists(srcfile):
-    message = 'OK, the "%s" file exists.'
-    print message % srcfile
-    #mv exists firmware to /home/factory/canaan_factory
-    mymovefile(srcfile,dstfile)
-    
-
-    #downloads MM821.mcs
-    url = 'https://canaan.io/downloads/software/avalon821/mm/latest/md5sums'
-    md5 = os.path.join('/home/factory/Avalon-extras/scripts/factory','md5sums' )
-
-    changelog = 'https://canaan.io/downloads/software/avalon821/mm/latest/changelog'
-    changeloglocal = os.path.join('/home/factory/Avalon-extras/scripts/factory','changelog' )
-
-    local = os.path.join('/home/factory/Avalon-extras/scripts/factory','MM821.mcs' )
-    urllib.urlretrieve(address,local,Schedule)
-
-    print "****************************************************************************"
-    print "Download the MD5 File"
-    print "****************************************************************************"
-    urllib.urlretrieve(url,md5,Schedule)
-
-    print "****************************************************************************"
-    print "Download the changelog"
-    print "****************************************************************************"
-    urllib.urlretrieve(changelog,changeloglocal,Schedule)
-
-
-    #check MD5
-    check1 = os.popen('cat md5sums | grep MM821.mcs | cut -c 1-32')
-    check2 = os.popen('md5sum MM821.mcs | cut -c 1-32')
-    ck1 =  check1.read()
-    ck2 =  check2.read()
-    #print ck1
-    #print ck2
-    check(ck1, ck2)
-
-
-else:
-    message = 'Sorry, I cannot find the "%s" file.'
-    print message % srcfile
-    
-
-    #downloads MM821.mcs
-    url = 'https://canaan.io/downloads/software/avalon821/mm/latest/md5sums'
-    md5 = os.path.join('/home/factory/Avalon-extras/scripts/factory','md5sums' )
-    
-    changelog = 'https://canaan.io/downloads/software/avalon821/mm/latest/changelog'
-    changeloglocal = os.path.join('/home/factory/Avalon-extras/scripts/factory','changelog' )
-
-    local = os.path.join('/home/factory/Avalon-extras/scripts/factory','MM821.mcs')
-    urllib.urlretrieve(address,local,Schedule)
-
-    print "****************************************************************************"
-    print "Download the MD5 File"
-    print "****************************************************************************"
-    urllib.urlretrieve(url,md5,Schedule)
-
-    print "****************************************************************************"
-    print "Download the changelog"
-    print "****************************************************************************"
-    urllib.urlretrieve(changelog,changeloglocal,Schedule)
-
-    #check MD5
-    check1 = os.popen('cat md5sums | grep MM821.mcs | cut -c 1-32')
-    check2 = os.popen('md5sum MM821.mcs | cut -c 1-32')
-    ck1 =  check1.read()
-    ck2 =  check2.read()
-    #print ck1
-    #print ck2
-    check(ck1, ck2)
-
-
-mychangelog = os.popen('cat changelog | grep Version | cut -c 9-23').read()
-print mychangelog
-
-
-#output = os.popen('ifconfig | grep eth | cut -c 39-65')
-#mac =  output.read()
+    if os.path.exists(srcfile):
+        message = 'OK, the "%s" file exists.'
+        print message % srcfile
+        #mv exists firmware to /home/factory/canaan_factory
+        mymovefile(srcfile,dstfile)
         
-#str = 'http://ams.b-bug.org/mac/?a=%s' %mac
-#print str
 
-#url = str +'&b=8211712-16c14b0' + '&c=%s'%ver  
-url = str +'&b=%s'%mychangelog + '&c=%s'%ver 
-print url
-webbrowser.open(url, new=0, autoraise=True)
+        #downloads MM821.mcs
+        url = 'https://canaan.io/downloads/software/avalon821/mm/latest/md5sums'
+        md5 = os.path.join('/home/factory/Avalon-extras/scripts/factory','md5sums' )
+
+        changelog = 'https://canaan.io/downloads/software/avalon821/mm/latest/changelog'
+        changeloglocal = os.path.join('/home/factory/Avalon-extras/scripts/factory','changelog' )
+
+        local = os.path.join('/home/factory/Avalon-extras/scripts/factory','MM821.mcs' )
+        urllib.urlretrieve(address,local,Schedule)
+
+        print "****************************************************************************"
+        print "Download the MD5 File"
+        print "****************************************************************************"
+        urllib.urlretrieve(url,md5,Schedule)
+
+        print "****************************************************************************"
+        print "Download the changelog"
+        print "****************************************************************************"
+        urllib.urlretrieve(changelog,changeloglocal,Schedule)
+
+
+        #check MD5
+        check1 = os.popen('cat md5sums | grep MM821.mcs | cut -c 1-32')
+        check2 = os.popen('md5sum MM821.mcs | cut -c 1-32')
+        ck1 =  check1.read()
+        ck2 =  check2.read()
+
+        check(ck1, ck2)
+
+
+    else:
+        message = 'Sorry, I cannot find the "%s" file.'
+        print message % srcfile
+        
+        url = 'https://canaan.io/downloads/software/avalon821/mm/latest/md5sums'
+        md5 = os.path.join('/home/factory/Avalon-extras/scripts/factory','md5sums' )
+        
+        changelog = 'https://canaan.io/downloads/software/avalon821/mm/latest/changelog'
+        changeloglocal = os.path.join('/home/factory/Avalon-extras/scripts/factory','changelog' )
+
+        local = os.path.join('/home/factory/Avalon-extras/scripts/factory','MM821.mcs')
+        urllib.urlretrieve(address,local,Schedule)
+
+        print "****************************************************************************"
+        print "Download the MD5 File"
+        print "****************************************************************************"
+        urllib.urlretrieve(url,md5,Schedule)
+
+        print "****************************************************************************"
+        print "Download the changelog"
+        print "****************************************************************************"
+        urllib.urlretrieve(changelog,changeloglocal,Schedule)
+
+        #check MD5
+        check1 = os.popen('cat md5sums | grep MM821.mcs | cut -c 1-32')
+        check2 = os.popen('md5sum MM821.mcs | cut -c 1-32')
+        ck1 =  check1.read()
+        ck2 =  check2.read()
+
+        check(ck1, ck2)
+
+
+    mychangelog = os.popen('cat changelog | grep Version | cut -c 9-23').read()
+    print mychangelog
+
+
+    #output = os.popen('ifconfig | grep eth | cut -c 39-65')
+    #mac =  output.read()
+            
+    #str = 'http://ams.b-bug.org/mac/?a=%s' %mac
+    #print str
+
+    #url = str +'&b=8211712-16c14b0' + '&c=%s'%ver  
+    url = str +'&b=%s'%mychangelog + '&c=%s'%ver 
+    print url
+    webbrowser.open(url, new=0, autoraise=True)
 
 
 
